6b8f74069f195122e739aeed8aa376d2e75b8d98
vagrant <vagrant@localhost.localdomain>
2019-03-23 01:58:45 +0000

    finish

--- scraping.php
+++ scraping.php
@@ -21,22 +21,22 @@ $max_page = $crawler->filter("body > main > div > div.dom-layout-01 > div > nav 
 for ($i = 1; $i <= $max_page; $i++) {
 	$url = $base_url . $place . "/?godate=" . $date . "&itemperpage=20&hotellistsort=recommend&staynight=1&room=1&roomassign=&mapdisp=0&planinfokeywordon=1&temproomassign=0&page=" . $i;
 	
-	echo$i . "ページ目:" . $url . PHP_EOL;
+echo$i . "ページ目:" . $url . PHP_EOL;
 
-	sleep(5);
+sleep(5);
 
-	$crawler = $client->request("GET",$url);
-	$crawler->filter("#spk_hotel-list > div > article")->each(function($article) use (&$result, &$place, &$date) {
+$crawler = $client->request("GET",$url);
+$crawler->filter("#spk_hotel-list > div > article")->each(function($article) use (&$result, &$place, &$date) {
 
-        $hotel_obj = new Hotel();
+$hotel_obj = new Hotel();
 
-        $price_obj = new Price();
+$price_obj = new Price();
 
         // 変数宣言
 
-        $name = $article->filter("div:nth-child(1) > div.domhotel-hotel-list__column-01 > h3 > a")->text();
+$name = $article->filter("div:nth-child(1) > div.domhotel-hotel-list__column-01 > h3 > a")->text();
 
-        echo $name.PHP_EOL;
+echo $name.PHP_EOL;
 
         $link = $article->filter("div.domhotel-hotel-list__column-01 > h3 > a")->attr("href");
 
@@ -53,9 +53,8 @@ for ($i = 1; $i <= $max_page; $i++) {
 if ($node_max_price->count() == 0) {
 $max_price = $min_price;
 } else {
-$min_price = $node_max_price->text();
+$max_price = $node_max_price->text();
 }
-
 $hotel_id = $hotel_obj->insertData([
 "name" => $name,"link" => "https://www.jtb.co.jp" . $link,
 
@@ -66,7 +65,7 @@ $hotel_id = $hotel_obj->insertData([
         ]);
 
 $search_char_list = ["円",",","～"];
-$replace_char_list = [“”, “”, “”];
+$replace_char_list = ["", "", ""];
 
         $price_obj->insertData([
 
